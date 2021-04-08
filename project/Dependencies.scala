fcfbf311b3174b0921b5bda3c1dee6c3b291f5cd
Scala Steward <me@scala-steward.org>
2021-04-08 20:08:53 +0200

    Update zio-config to 1.0.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val vAmq        = "5.16.0"
   val vLogback    = "1.2.3"
   val vZio        = "1.0.5"
-  val vZioConfig  = "1.0.2"
+  val vZioConfig  = "1.0.3"
   val vZioLogging = "0.5.8"
   val vZioNio     = "1.0.0-RC9"
 
